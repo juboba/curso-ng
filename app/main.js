f00add57ca522e0110aa4c17c92d9ed47d1b857e
Julio Borja Barra <juboba@gmail.com>
2016-08-27 17:37:20 +0000

    Agrega un controlador de ejemplo para el inbox.
    En el próximo paso reemplazaremos nuestro controlador por un componente.
    De esta manera encapsularemos la funcionalidad de inbox.

--- app/main.js
+++ app/main.js
@@ -1,1 +1,30 @@
 angular.module('clienteCorreo', [])
+
+    .controller('inboxCtrl', function() {
+        this.emails = [
+            {
+                id: 1,
+                asunto: 'Aprender AngularJS',
+                remitente: 'Rudolph Hess',
+                fecha: new Date()
+            },
+            {
+                id: 2,
+                asunto: 'Aprender AngularJS',
+                remitente: 'Winston Churchill',
+                fecha: new Date()
+            },
+            {
+                id: 3,
+                asunto: 'Aprender AngularJS',
+                remitente: 'Joseph Stalin',
+                fecha: new Date()
+            },
+            {
+                id: 4,
+                asunto: 'Aprender AngularJS',
+                remitente: 'Theodore Roosevelt',
+                fecha: new Date()
+            }
+        ];
+    });
