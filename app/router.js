303fbc3dc7fee4c2d88c32ce083f30b22045919d
Julio Borja Barra <juboba@gmail.com>
2016-08-28 12:28:18 +0000

    Cambia controller innecesario por un resolve.

--- app/router.js
+++ app/router.js
@@ -7,23 +7,32 @@ angular
                 template: '<cc-inbox></cc-inbox>'
             })
 
-        /*
-         * Agregamos una ruta con un parámetro `id` que vamos a usar para
-         * Encontrar el mail específico:
-         */
             .when('/mail/:id', {
-                template: 'Hello Mail {{mail.routeId}}!',
-                controller: function($routeParams) {
-                    this.routeId = $routeParams.id;
-                },
                 /*
-                 * Notar que usamos (al igual que cuando usamos un controlador
-                 * solo) la sintaxis 'controller as'.
-                 * Anteriormente: `ng-controller="someCtrl as some"` en la vista
-                 * Ahora: controllerAs: 'some' en la definición de la ruta.
-                 * Ambos producen el mismo resultado.
+                 * Ahora cambiamos el controller por un `resolve`.
+                 * Esto permite que `$resolve.mail` esté disponible justo
+                 * antes de cambiar de ruta:
+                 * (Es decir, nuestra ruta nunca se cargará con datos vacíos,
+                 * como si ocurre en `/inbox`, los datos se cargan después de
+                 * cargar la ruta [en el componente]).
+                 * Por ahora sólo mostramos el json de la respuesta
+                 * (usando un filtro)
                  */
-                controllerAs: 'mail'
+                template: '{{$resolve.mail | json}}',
+                /*
+                 * Podemos agregar tantos 'resolves' como queramos.
+                 * Estarán disponibles en el ámbito de la ruta como:
+                 * $resolve.[nombre]
+                 */
+                resolve: {
+                    mail: function(mailSrv, $route) {
+                        /*
+                         * El valor no será la promesa que devolvemos
+                         * sino el valor 'resuelto' de la promesa:
+                         */
+                        return mailSrv.getOne($route.current.params.id);
+                    }
+                }
             })
 
             .otherwise('/inbox');
